dee5374d113c6bf3380af6e2bf05fdb720ead7f2
aadtyaraj01 <aadtyaraj01@gmail.com>
2024-07-21 17:42:12 +0530

    menu created

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,35 @@
 <script>
+	import { NavBar, NavCta } from '$lib/Nav';
 	import '../app.css';
+
+	let { children } = $props();
 </script>
 
-<slot></slot>
+<!--  
+	<Nav>
+		{@render icon()}
+		<NavPanel>
+			
+	</NavPanel>
+	
+	<NavCta>
+		<NavCtaLink></NavCtaLink>
+	</NavCta>
+</Nav>
+-->
+
+<NavBar>
+	{#snippet icon()}
+		<a href="/">🍻</a>
+	{/snippet}
+
+	<NavCta>
+		<li><a href="/">📞</a></li>
+		<li><a href="/">📲</a></li>
+		<li><a href="/">🦆</a></li>
+	</NavCta>
+</NavBar>
+
+{@render children()}
 
 <style></style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Accordion, AccordionItem } from '$lib/Accordion';
+	import { faqs } from '$lib/faqs.js';
+	import { Nav } from '$lib/Nav';
+</script>
+
+<div class="container mx-auto mt-10 max-w-3xl">
+	<Accordion>
+		{#each faqs as { question, answer }, index}
+			<AccordionItem {index} open={index === 0}>
+				{#snippet summary({ defaultStyles })}
+					<h3 class={defaultStyles}>{question}</h3>
+				{/snippet}
+				{#snippet description()}
+					<p>{answer}</p>
+				{/snippet}
+			</AccordionItem>
+		{/each}
+	</Accordion>
+</div>
